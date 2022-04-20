f1b068b7ef1fa7500d1a282f48bbcee6fc9d5d1f
Claudio Muñoz <claudio.munoz.m@usach.cl>
2022-04-20 00:20:04 -0400

    Se crea la base para la aplicacion web

---
+++ src/test/java/com/mingeso/mingeso/MingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingeso.mingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
